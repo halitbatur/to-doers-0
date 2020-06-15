77e73e486a1661b1ee98c2464a089b6f40193bbd
Bilge <lagala999@gmail.com>
2020-06-15 23:01:08 +0300

    added a switch to the board container, switches between listview and boardview

--- src/Containers/boardContainer/BoardContainer.js
+++ src/Containers/boardContainer/BoardContainer.js
@@ -3,11 +3,17 @@ import { useSelector } from "react-redux";
 import AddBtn from "../../Components/addBtn/AddBtn";
 import SingleBoard from "../../Components/singleBoard/SingleBoard";
 import SingleList from "../../Components/singleList/SingleList";
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+
 
 export default function BoardContainer(props) {
   const db = useSelector((state) => state.value);
-
+  const [isOnListView, shouldSetViewToList] = useState(false);
   const [boards, setBoards] = useState([]);
+  const [switchState, setState] = React.useState(false);
+
 
   // Live updates the boards
   const liveUpdate = async () => {
@@ -39,15 +45,22 @@ export default function BoardContainer(props) {
     setBoards(() => boards.filter((board) => board.id !== id));
   };
 // Hello World
-  const renderDemBoards = () => {
+  const renderDemBoards = (isOnListView) => {
     return boards.map((board) => {
+      console.log(board)
       return (
+        isOnListView?
         <SingleList
           data={board.data()}
           boardId={board.id}
           key={board.id}
           deleteBoard={deleteBoard}
-        ></SingleList>
+          ></SingleList> : <SingleBoard
+            data={board.data()}
+            boardId={board.id}
+            key={board.id}
+            deleteBoard={deleteBoard}
+          ></SingleBoard>
       );
     });
   };
@@ -69,7 +82,16 @@ export default function BoardContainer(props) {
           display: "grid",
         }}
       >
-        {renderDemBoards()}
+        {renderDemBoards(isOnListView)}
+        
+          <FormControlLabel
+            control={<Switch checked={switchState} onChange={()=>{
+            shouldSetViewToList(!isOnListView);
+            renderDemBoards(isOnListView);
+            setState(!switchState);
+            }} name="checkedA" />}
+            label="Switch Look"
+          />
       </div>
 
       <AddBtn />
